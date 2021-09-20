e37f039e358e8711d68ac503b6a0d1d4034ff05f
Igor Arinichev <igor.arin@gmail.com>
2021-09-20 23:43:32 +0300

    init: start museum task from stage0 project

---
+++ museum/app/js/main.js
@@ -0,0 +1,15 @@
+// $(function () {});
+
+const progress = document.querySelector(".progress-bar");
+
+progress.addEventListener("input", function () {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`;
+});
+
+const progress2 = document.querySelector(".volume-bar");
+
+progress2.addEventListener("input", function () {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`;
+});
