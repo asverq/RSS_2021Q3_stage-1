2eb04c46677dab24cd4a56feb4a05beed6904fac
Igor Arinichev <igor.arin@gmail.com>
2021-10-04 17:58:56 +0300

    feat: implement adaptive layout

---
+++ museum-adaptive/js/main.js
@@ -0,0 +1,227 @@
+const owerlayBody = document.querySelector('.popup-owerlay-transparent');
+const headerMenu = document.querySelector('.header-nav__sub-wrap');
+const headerMenuLink = document.querySelectorAll('.header-nav__link');
+const burger = document.querySelector('.header-nav__burger');
+const burgerLine = document.querySelector('.line');
+const welcomeSlogan = document.querySelector('.welcome__slogan-wrap');
+burger.addEventListener('click', () => {
+  headerMenu.classList.toggle('active');
+  burgerLine.classList.toggle('active');
+  welcomeSlogan.classList.toggle('active');
+  owerlayBody.classList.toggle('active');
+})
+
+headerMenuLink.forEach(item => [
+  item.addEventListener('click', () => {
+    headerMenu.classList.remove('active');
+    burgerLine.classList.remove('active');
+    welcomeSlogan.classList.remove('active');
+    owerlayBody.classList.remove('active');
+  })
+])
+owerlayBody.addEventListener('click', () => {
+  headerMenu.classList.remove('active');
+  burgerLine.classList.remove('active');
+  welcomeSlogan.classList.remove('active');
+  owerlayBody.classList.remove('active');
+})
+
+const counter = document.querySelector('.counter__left');
+
+$(function () {
+  $('.welcome-slider').slick({
+    infinite: true,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 7000,
+    speed: 600,
+    appendArrows: $('.welcome-slider__arrows-wrapper'),
+    prevArrow: '<button class="welcome-slider__arrows arrow-left"></button>',
+    nextArrow: '<button class="welcome-slider__arrows arrow-right"></button>',
+    appendDots: $('.welcome-slider__dots'),
+  });
+});
+
+$('.welcome-slider').on('afterChange', function (event, slick, currentSlide, nextSlide) {
+  counter.innerText = `0${currentSlide + 1}`
+});
+
+$(function () {
+  $('.video-slider').slick({
+    slidesToShow: 3,
+    infinite: true,
+    dots: true,
+    autoplaySpeed: 7000,
+    speed: 600,
+    appendArrows: $('.video-slider__dots-wrapper'),
+    appendDots: $('.dots-subwrap'),
+    responsive: [{
+      breakpoint: 769,
+      settings: {
+        slidesToShow: 2,
+      }
+    }, ],
+  });
+});
+
+//Random Gallery
+const galleryImg = document.querySelectorAll('.gallery__img');
+const rndArr = new Array(galleryImg.length).fill(1);
+
+for (let i = 1; i < galleryImg.length; i++) {
+  rndArr[i] = rndArr[i - 1] + 1;
+}
+
+shuffle(rndArr);
+
+for (let x = 0; x < galleryImg.length; x++) {
+  galleryImg[x].setAttribute('src', `img/gallery/gallery${rndArr[x]}.webp`);
+}
+
+function shuffle(array) {
+  array.sort(() => Math.random() - 0.5);
+}
+//END__Random Gallery
+
+//BA-Slider
+const
+  baSlider = document.querySelector('.ba-slider'),
+  baSeparator = document.querySelector('.ba-slider__separator')
+
+let
+  baImgWrap = document.querySelector('.ba-slider__image-wrap')
+
+baSlider.addEventListener('mousedown', moveStart, false);
+baSlider.addEventListener('mouseup', moveEnd, false);
+
+let baPosX
+
+function moveStart(e) {
+  console.dir(e.target);
+  e.preventDefault();
+  baPosX = e.offsetX
+  baImgWrap.style.width = `${baPosX}px`
+  baSeparator.style.left = `${baPosX}px`
+  baSlider.addEventListener('mousemove', moveAction);
+}
+
+function moveAction(e) {
+  e.preventDefault();
+  baPosX = e.offsetX
+  baImgWrap.style.width = `${baPosX}px`
+  baSeparator.style.left = `${baPosX}px`
+}
+
+function moveEnd() {
+  baSlider.removeEventListener('mousemove', moveAction);
+}
+//END__BA-Slider
+
+//Popup-form
+const buyNowBtn = document.querySelector('.calculator-btn-sum');
+const popupForm = document.querySelector('.tickets__popup-form');
+const popupFormClose = document.querySelector('.popup-form__close-btn');
+const popupOwerlay = document.querySelector('.popup-owerlay');
+buyNowBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  popupForm.classList.remove('hide');
+  popupOwerlay.classList.add('active');
+})
+
+popupFormClose.addEventListener('click', () => {
+  popupForm.classList.add('hide');
+  popupOwerlay.classList.remove('active');
+})
+
+popupOwerlay.addEventListener('click', () => {
+  popupForm.classList.add('hide');
+  popupOwerlay.classList.remove('active');
+})
+//END__Popup-form
+
+//Ripple Effect
+const bookBtn = document.querySelector('.result-card__button');
+
+bookBtn.addEventListener('click', function (e) {
+  const x = e.offsetX;
+  const y = e.offsetY;
+  const circle = document.createElement('span');
+
+  circle.classList.add('circle');
+  circle.style.top = y + 'px';
+  circle.style.left = x + 'px';
+
+  this.appendChild(circle);
+
+  setTimeout(() => circle.remove(), 500);
+})
+//END__Ripple Effect
+
+console.log(`
+Моя самоопроверка:
+
+Выполненные пункты:___________________________________________________________________
+
+Вёрстка соответствует макету. Ширина экрана 1024px +40
+  Блок header +4
+  Секция Welcome +4
+  Секция Visiting +4
+  Секция Explore +4
+  Секция Video +4
+  Секция Gallery +4
+  Секция Tickets +4
+  Форма покупки билетов +4
+  Секция Contacts +4
+  Блок footer +4
+
+Вёрстка соответствует макету. Ширина экрана 768px +40
+  Блок header +4
+  Секция Welcome +4
+  Секция Visiting +4
+  Секция Explore +4
+  Секция Video +4
+  Секция Gallery +4
+  Секция Tickets +4
+  Форма покупки билетов +4
+  Секция Contacts +4
+  Блок footer +4
+
+Вёрстка соответствует макету. Ширина экрана 420px +40
+  Блок header +4
+  Секция Welcome +4
+  Секция Visiting +4
+  Секция Explore +4
+  Секция Video +4
+  Секция Gallery +4
+  Секция Tickets +4
+  Форма покупки билетов +4
+  Секция Contacts +4
+  Блок footer +4
+
+Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки +6
+
+Совмещается адаптивная и респонсивная (резиновая) вёрстка +14 При изменении ширины экрана плавно изменяются размеры:
+  слайдера в секции Welcome +2
+  слайдера сравнения изображений в секции Explore +2
+  кастомного видеоплеера в секции Video +2
+  слайдера в секции Video +2
+  YouTube-видео в плейлисте в секции Video, маленькие видео выровнены по краям большого +2
+  галереи изображений и изображений в ней +2
+  карты +2
+
+На ширине экрана 1024рх и меньше реализовано адаптивное меню +12
+  при нажатии на бургер-иконку меню появляется, плавно выдвигаясь слева, бургер-иконка изменяется на крестик. При нажатии на крестик меню исчезает, плавно возвращаясь назад, иконка крестика превращается в бургер-иконку +2
+  ссылки в меню работают, обеспечивая плавную прокрутку по якорям +2
+  при клике по ссылке в адаптивном меню, или при клике по любому месту сайта, кроме самого адаптивного меню, меню закрывается +2
+  вёрстка меню соответствует макету на всех проверяемых разрешениях +6
+
+Не выполненные пункты:_______________________________________________________________
+
+Оптимизация скорости загрузки страницы +8 Оптимизация скорости загрузки страницы определяется при помощи сервиса https://developers.google.com/speed/pagespeed/insights/. Результат проверки скорости сайта для мобильных устройств:
+  0 to 49 (red): Poor - не выполнено, 0 баллов
+  50 to 89 (orange): Needs Improvement - частично выполнено +4
+  90 to 100 (green): Good - выполнено полностью +8
+
+
+Итого, по подсчёту баллов выполненных пунктов: 150 баллов из возможных 150
+`);
---
+++ museum-adaptive/js/video.js
@@ -0,0 +1,270 @@
+const
+  container = document.querySelector('.video-player__container'),
+  media = document.querySelector('.video-player__video'),
+  controlsBody = document.querySelector('.controls-body'),
+  controls = document.querySelector('.video-player__bar'),
+  screenBtn = document.querySelector('.btn-on-screen'),
+  playBtn = document.querySelector('.play-btn'),
+  // rwdBtn = document.querySelector('.prev-btn'),
+  // fwdBtn = document.querySelector('.next-btn'),
+  volumeBtn = document.querySelector('.btn-volume'),
+  fullscreenBtn = document.querySelector('.btn-fullscreen'),
+  volumeBar = document.querySelector(".volume-bar"),
+  progressBar = document.querySelector(".progress-bar"),
+  // timerBar = document.querySelector(".timer"),
+  speedBar = document.querySelector(".speed"),
+  videoTitle = document.querySelector('.video-title')
+
+const mediaArr = [
+  'video-1',
+  'video-2',
+  'video-3',
+  'video-4',
+  'video-5',
+];
+
+let activeMedia = 0;
+let fullsreenMode = false;
+let isPlaying = false;
+let isPaused = false;
+
+function setVideoTitle() {
+  videoTitle.innerText = mediaArr[activeMedia];
+}
+setVideoTitle();
+
+function setPlayerReady() {
+  media.volume = volumeBar.value;
+  volumeBar.style.background = `
+    linear-gradient(
+      to right,
+      #710707 0%,
+      #710707 ${volumeBar.value * 100}%,
+      #C4C4C4 ${volumeBar.value * 100}%,
+      #C4C4C4 100%
+    )
+  `;
+}
+setPlayerReady();
+
+let isShiftDown = false;
+
+media.addEventListener('ended', stopMedia);
+media.addEventListener('ended', autoNextMedia);
+media.addEventListener('click', playPauseMedia);
+screenBtn.addEventListener('click', playPauseMedia);
+// rwdBtn.addEventListener('click', function () {
+//   changeMedia('rwd')
+// });
+// fwdBtn.addEventListener('click', function () {
+//   changeMedia('fwd')
+// });
+playBtn.addEventListener('click', playPauseMedia);
+volumeBtn.addEventListener('click', toggleMute);
+fullscreenBtn.addEventListener('click', toggleFullScreen)
+document.addEventListener('keydown', (e) => {
+  if (e.code === 'Space') {
+    e.preventDefault();
+    playPauseMedia();
+  }
+  if (e.code === 'KeyF') {
+    e.preventDefault();
+    toggleFullScreen();
+  }
+  if (e.code === 'KeyM') {
+    e.preventDefault();
+    toggleMute();
+  }
+  if (e.code === 'ShiftLeft' || e.code === 'ShiftRight') {
+    e.preventDefault();
+    isShiftDown = true;
+  }
+  if (e.code === 'Period' && isShiftDown === true) {
+    e.preventDefault();
+    speedPlay('up');
+  }
+  if (e.code === 'Comma' && isShiftDown === true) {
+    e.preventDefault();
+    speedPlay('down');
+  }
+});
+
+document.addEventListener('keyup', (e) => {
+  if (e.code === 'ShiftLeft' || e.code === 'ShiftRight') {
+    e.preventDefault();
+    isShiftDown = false;
+  }
+})
+
+function changeMedia(order) {
+  if (order === 'rwd') {
+    if (activeMedia > 0) {
+      activeMedia -= 1;
+      media.src = `video/video-${activeMedia + 1}.mp4`;
+      setVideoTitle();
+      if (isPlaying && !isPaused) {
+        playPauseMedia();
+        return;
+      }
+      setTimeout(() => {
+        progressBar.value = '0';
+        progressBar.style.background = '#c4c4c4';
+      }, 0)
+    }
+  } else if (order === 'fwd') {
+    if (activeMedia < mediaArr.length - 1) {
+      activeMedia += 1;
+      media.src = `video/video-${activeMedia + 1}.mp4`;
+      setVideoTitle();
+      if (isPlaying && !isPaused) {
+        playPauseMedia();
+        return;
+      }
+      setTimeout(() => {
+        progressBar.value = '0';
+        progressBar.style.background = '#c4c4c4';
+      }, 0)
+    }
+  }
+}
+
+function speedPlay(arg) {
+  if (arg === 'up') {
+    if (media.playbackRate < 2.5) {
+      media.playbackRate += 0.25;
+      // speedBar.innerText = `${media.playbackRate}x`;
+    }
+  }
+  if (arg === 'down') {
+    if (media.playbackRate > 0.25) {
+      media.playbackRate -= 0.25;
+      // speedBar.innerText = `${media.playbackRate}x`;
+    }
+  }
+}
+
+document.addEventListener('fullscreenchange', setPlayerFullscreenStyles);
+
+function setPlayerFullscreenStyles() {
+  if (fullsreenMode === false) {
+    controlsBody.classList.add('fullscreen');
+    controls.classList.add('fullscreen');
+    fullsreenMode = true;
+  } else if (fullsreenMode === true) {
+    controlsBody.classList.remove('fullscreen');
+    controls.classList.remove('fullscreen');
+    fullsreenMode = false;
+  }
+}
+
+function toggleFullScreen() {
+  if (!document.fullscreenElement) {
+    container.requestFullscreen();
+  } else if (document.exitFullscreen) {
+    document.exitFullscreen();
+  }
+}
+
+function playPauseMedia() {
+  if (media.paused) {
+    playBtn.children[0].setAttribute('class', '_icon-pause-btn icon');
+    screenBtn.style.display = 'none';
+    media.play();
+    isPlaying = true;
+    isPaused = false;
+  } else {
+    playBtn.children[0].setAttribute('class', '_icon-play-btn icon');
+    screenBtn.style.display = 'block';
+    media.pause();
+    isPaused = true;
+  }
+}
+
+function stopMedia() {
+  media.currentTime = 0;
+  playBtn.children[0].setAttribute('class', '_icon-play-btn icon');
+  screenBtn.style.display = 'block';
+  // speedBar.innerText = '1x';
+  isPlaying = false;
+}
+
+function autoNextMedia() {
+  if (activeMedia === mediaArr.length - 1) {
+    stopMedia();
+    return;
+  }
+  changeMedia('fwd');
+  playPauseMedia();
+}
+
+function toggleMute() {
+  if (media.muted) {
+    volumeBtn.children[0].setAttribute('class', '_icon-volume-btn icon');
+    media.muted = false;
+  } else {
+    volumeBtn.children[0].setAttribute('class', '_icon-volume-btn_mute icon');
+    media.muted = true;
+  }
+}
+
+//progress bar logics
+media.addEventListener('timeupdate', updateProgressBar);
+progressBar.addEventListener('input', liveSearchProgress);
+
+function liveSearchProgress() {
+  const value = this.value;
+  media.currentTime = (value * media.duration) / 100;
+  this.style.background = `
+    linear-gradient(
+      to right,
+      #710707 0%,
+      #710707 ${value}%,
+      #C4C4C4 ${value}%,
+      #C4C4C4 100%
+    )
+  `;
+}
+
+// function timer() {
+//   const minutes = Math.floor(media.currentTime / 60);
+//   const hours = Math.floor(minutes / 60) % 24;
+//   const seconds = Math.floor(media.currentTime % 60);
+//   timerBar.innerText = `${
+//     String(hours).padStart('2', '0')
+//   }:${
+//     String(minutes).padStart('2', '0')
+//   }:${
+//     String(seconds).padStart('2', '0')
+//   }`;
+// }
+
+function updateProgressBar() {
+  const position = (media.currentTime / media.duration) * 100;
+  progressBar.value = position;
+  progressBar.style.background = `
+    linear-gradient(
+      to right,
+      #710707 0%,
+      #710707 ${position}%,
+      #C4C4C4 ${position}%,
+      #C4C4C4 100%
+    )
+  `;
+  // timer();
+}
+
+volumeBar.addEventListener("input", setVolume);
+
+function setVolume() {
+  const value = this.value;
+  media.volume = value;
+  this.style.background = `
+    linear-gradient(
+      to right,
+      #710707 0%,
+      #710707 ${value * 100}%,
+      #C4C4C4 ${value * 100}%,
+      #C4C4C4 100%
+    )
+  `;
+}
