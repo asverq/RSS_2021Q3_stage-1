897624260fc9f0908d474fa5fc59516b8b15f466
Igor Arinichev <igor.arin@gmail.com>
2021-10-21 16:00:18 +0300

    feat: implement clock and current date

--- momentum/app/js/main.js
+++ momentum/app/js/main.js
@@ -1,6 +1,72 @@
-$(function(){
+const time = document.querySelector('.time');
+const date = document.querySelector('.date');
 
+let loc = 'RU';
 
+const locFormat = {
+    'EN': 'en-EN',
+    'RU': 'ru-RU',
+    'month': {
+        'EN': [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December'
+        ],
+        'RU': [
+            'Январь',
+            'Февраль',
+            'Март',
+            'Апрель',
+            'Май',
+            'Июнь',
+            'Июль',
+            'Август',
+            'Сентябрь',
+            'Октябрь',
+            'Ноябрь',
+            'Декабрь'
+        ]
+    },
+    'day': {
+        'EN': [
+            'Sunday',
+            'Monday',
+            'Tuesday',
+            'Wednesday',
+            'Thursday',
+            'Friday',
+            'Saturday'
+        ],
+        'RU': [
+            'Воскресенье',
+            'Понедельник',
+            'Вторник',
+            'Среда',
+            'Четверг',
+            'Пятница',
+            'Суббота'
+        ]
+    }
+}
 
-  
-});
+function setDate() {
+    let currentMonth = locFormat.month[loc][new Date().getMonth()];
+    let currentWeekDay = locFormat.day[loc][new Date().getDay()];
+    date.textContent = `${currentWeekDay}, ${currentMonth} ${new Date().getDate()}`
+}
+
+function showTime() {
+    time.textContent = new Date().toLocaleTimeString(locFormat[loc]);
+    setDate();
+    setTimeout(showTime, 1000);
+}
+showTime();
