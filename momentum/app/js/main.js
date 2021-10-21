0bcb8b302dfaf8951fa21f2f93268d5b59b126da
Igor Arinichev <igor.arin@gmail.com>
2021-10-21 19:01:31 +0300

    feat: implement greeting

--- momentum/app/js/main.js
+++ momentum/app/js/main.js
@@ -1,72 +1,75 @@
 const time = document.querySelector('.time');
 const date = document.querySelector('.date');
+const greeting = document.querySelector('.greeting');
+const userName = document.querySelector('.name');
 
-let loc = 'RU';
+let loc = 'EN';
 
 const locFormat = {
-    'EN': 'en-EN',
-    'RU': 'ru-RU',
-    'month': {
-        'EN': [
-            'January',
-            'February',
-            'March',
-            'April',
-            'May',
-            'June',
-            'July',
-            'August',
-            'September',
-            'October',
-            'November',
-            'December'
-        ],
-        'RU': [
-            'Январь',
-            'Февраль',
-            'Март',
-            'Апрель',
-            'Май',
-            'Июнь',
-            'Июль',
-            'Август',
-            'Сентябрь',
-            'Октябрь',
-            'Ноябрь',
-            'Декабрь'
-        ]
-    },
-    'day': {
-        'EN': [
-            'Sunday',
-            'Monday',
-            'Tuesday',
-            'Wednesday',
-            'Thursday',
-            'Friday',
-            'Saturday'
-        ],
-        'RU': [
-            'Воскресенье',
-            'Понедельник',
-            'Вторник',
-            'Среда',
-            'Четверг',
-            'Пятница',
-            'Суббота'
-        ]
-    }
+  'EN': 'en-EN',
+  'RU': 'ru-RU',
+  'month': {
+    'EN': [
+      'January', 'February', 'March', 'April', 'May', 'June',
+      'July', 'August', 'September', 'October', 'November', 'December'
+    ],
+    'RU': [
+      'Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
+      'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'
+    ]
+  },
+  'day': {
+    'EN': [
+      'Sunday', 'Monday', 'Tuesday', 'Wednesday',
+      'Thursday', 'Friday', 'Saturday'
+    ],
+    'RU': [
+      'Воскресенье', 'Понедельник', 'Вторник', 'Среда',
+      'Четверг', 'Пятница', 'Суббота'
+    ]
+  },
+  'timeOfDay': {
+    'EN': [
+      'Good morning',
+      'Good afternoon',
+      'Good evening',
+      'Good night'
+    ],
+    'RU': [
+      'Доброе утро',
+      'Добрый день',
+      'Добрый вечер',
+      'Доброй ночи'
+    ]
+  }
 }
 
 function setDate() {
-    let currentMonth = locFormat.month[loc][new Date().getMonth()];
-    let currentWeekDay = locFormat.day[loc][new Date().getDay()];
-    date.textContent = `${currentWeekDay}, ${currentMonth} ${new Date().getDate()}`
+  let currentMonth = locFormat.month[loc][new Date().getMonth()];
+  let currentWeekDay = locFormat.day[loc][new Date().getDay()];
+  date.textContent = `${currentWeekDay}, ${currentMonth} ${new Date().getDate()}`
+}
+
+function setGreeting() {
+  const currTime = new Date().getHours();
+  if (currTime >= 6 && currTime < 12) greeting.textContent = locFormat.timeOfDay[loc][0];
+  if (currTime >= 12 && currTime < 18) greeting.textContent = locFormat.timeOfDay[loc][1];
+  if (currTime >= 18 && currTime < 24) greeting.textContent = locFormat.timeOfDay[loc][2];
+  if (currTime >= 0 && currTime < 6) greeting.textContent = locFormat.timeOfDay[loc][3];
 }
 
 function showTime() {
-    time.textContent = new Date().toLocaleTimeString(locFormat[loc]);
-    setDate();
-    setTimeout(showTime, 1000);
+  time.textContent = new Date().toLocaleTimeString(locFormat[loc]);
+  setDate();
+  setGreeting()
+  setTimeout(showTime, 1000);
 }
 showTime();
+
+userName.addEventListener('input', () => {
+  localStorage.setItem('userName', userName.value)
+})
+
+if (localStorage.getItem('userName')) {
+  userName.value = localStorage.getItem('userName');
+}
