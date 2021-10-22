647db3adf36175b51ab3db96eab8d2a4c8b5af0f
Igor Arinichev <igor.arin@gmail.com>
2021-10-22 10:49:36 +0300

    feat: implement background slider

--- momentum/app/js/main.js
+++ momentum/app/js/main.js
@@ -1,9 +1,18 @@
+const body = document.querySelector('body');
 const time = document.querySelector('.time');
 const date = document.querySelector('.date');
 const greeting = document.querySelector('.greeting');
 const userName = document.querySelector('.name');
+const sliderBtns = document.querySelector('.slider-icons');
 
 let loc = 'EN';
+let rndNumber;
+
+function randomNum() {
+  rndNumber = Math.floor(Math.random() * 20) + 1;
+  console.log(rndNumber);
+}
+randomNum();
 
 const locFormat = {
   'EN': 'en-EN',
@@ -44,10 +53,26 @@ const locFormat = {
   }
 }
 
+function debounce(func, wait = 20, immediate = true) {
+  let timeout;
+  return function () {
+    let context = this,
+      args = arguments;
+    let later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    }
+    let callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  }
+}
+
 function setDate() {
   let currentMonth = locFormat.month[loc][new Date().getMonth()];
   let currentWeekDay = locFormat.day[loc][new Date().getDay()];
-  date.textContent = `${currentWeekDay}, ${currentMonth} ${new Date().getDate()}`
+  date.textContent = `${currentWeekDay}, ${currentMonth} ${new Date().getDate()}`;
 }
 
 function setGreeting() {
@@ -61,7 +86,7 @@ function setGreeting() {
 function showTime() {
   time.textContent = new Date().toLocaleTimeString(locFormat[loc]);
   setDate();
-  setGreeting()
+  setGreeting();
   setTimeout(showTime, 1000);
 }
 showTime();
@@ -73,3 +98,46 @@ userName.addEventListener('input', () => {
 if (localStorage.getItem('userName')) {
   userName.value = localStorage.getItem('userName');
 }
+
+sliderBtns.addEventListener('click', debounce(changeSlide, 700));
+
+function getDayTime() {
+  const currTime = new Date().getHours();
+  if (currTime >= 6 && currTime < 12) return 'morning';
+  if (currTime >= 12 && currTime < 18) return 'afternoon';
+  if (currTime >= 18 && currTime < 24) return 'evening';
+  if (currTime >= 0 && currTime < 6) return 'night';
+}
+
+function changeSlide(e) {
+  if (e.target === document.querySelector('.slide-prev')) {
+    if (rndNumber === 1) {
+      rndNumber = 20;
+    } else {
+      rndNumber -= 1;
+    }
+    console.log(rndNumber);
+    setBg();
+  }
+  if (e.target === document.querySelector('.slide-next')) {
+    if (rndNumber === 20) {
+      rndNumber = 1;
+    } else {
+      rndNumber += 1;
+    }
+    console.log(rndNumber);
+    setBg();
+  }
+}
+
+function setBg() {
+  let bgNum;
+  rndNumber < 10 ? bgNum = `0${rndNumber}` : bgNum = `${rndNumber}`;
+  const img = new Image();
+  img.src = `https://raw.githubusercontent.com/asverq/stage1-tasks/assets/images/${getDayTime()}/compressed/${bgNum}.webp`;
+  img.onload = () => {
+    body.style.backgroundImage = `url('${img.src}')`;
+  };
+}
+setBg();
+
